298cce9f36e3bfe1b4964a7cc37c428ca8f0b1e5
Sean Bocirnea <sean@aetheris.ca>
2022-03-29 20:28:29 -0700

    Add query samples

---
+++ public/join_query.php
@@ -0,0 +1,45 @@
+<!-- NOT DONE -->
+
+<?php require "templates/header.php"; ?>
+
+<form method="post">
+  <label for="column">Column</label>
+  <input type="text" name="column" id="column">
+  <input type="text" name="column" value="column">
+</form>
+
+<?php
+
+require "data/config.php";
+
+try {
+    // Create connection
+    $connection = new PDO($dsn, $username, $password, $options);
+    
+    $col = $_POST["column"];
+
+    $sql = "SELECT " . $_POST["column"] . " FROM Venues";
+    $result = $connection->query($sql);
+
+    if ($col != "address" and $col != "name" and $col != "capacity") {
+        echo "Invalid column.";
+    } else if ($_POST["column"] == "name") {
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<br>". $row[$col] . "<br>";
+            }
+        } else {
+            echo "No results found.";
+        }
+
+    }
+    $connection->close();
+
+} catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+}
+?>
+
+<?php require "templates/footer.php"; ?>
--- public/select_query.php
+++ public/select_query.php
@@ -1,26 +1,32 @@
 <?php require "templates/header.php"; ?>
 
+<form method="post">
+  <label for="total_goals">Total Goals</label>
+  <input type="number" name="total_goals" id="total_goals">
+  <input type="number" name="ngoals" value="ngoals">
+</form>
+
 <?php
+
 require "data/config.php";
 
 try {
     // Create connection
     $connection = new PDO($dsn, $username, $password, $options);
 
-
-    $sql = "SELECT id, firstname, lastname FROM Players";
+    $sql = "SELECT P.firstname, P.lastname FROM Players P WHERE P.total_goals >= " . $_POST["ngoals"];
     $result = $connection->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo "<br>". $row["firstname"]. " " . $row["lastname"] . "<br>";
+            echo "<br>". $row["firstname"] . " " . $row["lastname"] . "<br>";
         }
     } else {
         echo "No results found.";
     }
-
-$connection->close();
+    $connection->close();
+    
 } catch(PDOException $error) {
     echo $sql . "<br>" . $error->getMessage();
 }
