1d9a98b4d2922cdb89ec257de9221f743f524b00
Sean Bocirnea <sean@aetheris.ca>
2022-03-31 14:52:42 -0700

    add projection

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@
     <li> <a href="delete.php"><strong>Delete</strong></a> - delete a rink </li>
     <li> <a href="update.php"><strong>Update</strong></a> - rename a team </li>
     <li> <a href="join_query.php"><strong>Join</strong></a> - find size of rinks </li>
-    <li> <a href="projection_query.php"><strong>Projection</strong></a> </li>
+    <li> <a href="projection_query.php"><strong>Projection</strong></a> - project venues </li>
     <li> <a href="select_query.php"><strong>Select</strong></a> - Select players with more than n goals </li>
     <li> <a href="aggregation.php"><strong>Aggregation</strong></a> - salaried player with the lowest salary </li>
-    <li> <a href="nested_aggregation.php"><strong>Nested Aggregation</strong></a> - average goals per position</li>
+    <li> <a href="nested_aggregation.php"><strong>Nested Aggregation</strong></a> - average goals per position </li>
     <!-- <li> <a href="read.php"><strong>Read</strong></a> - find a user </li> -->
 </ul>
 
--- public/projection_query.php
+++ public/projection_query.php
@@ -2,8 +2,13 @@
 
 <form method="post">
   <label for="column">Column</label>
-  <input type="text" name="column" value="column">
-  <input type="submit" name="submit" value="Submit">
+  <select name="column">
+    <option value="address">Address</option>
+    <option value="name">Name</option>
+    <option value="capacity">Capacity</option>
+    <option value="city">City</option>
+  </select>
+  <input type="submit" name="submit" value="View Results">
 </form>
 
 <?php
@@ -21,25 +26,20 @@ if (isset($_POST['submit'])) {
         // Create connection
         $connection = new PDO($dsn, $username, $password, $options);
 
-        $sql = "SELECT :column FROM Venues";
         $col = $_POST['column'];
+        $sql = "SELECT $col FROM Venues";
 
         $result = $connection->prepare($sql);
-        $result->bindParam(':column', $col, PDO::PARAM_STR);
         $result->execute();
 
         $data = $result->fetchAll();
-
-        echo $col;
 
         if ($data && $result->rowCount() > 0) { ?>
             <h2>Results</h2>
         
             <table>
               <thead>
-                <tr>
-                <?php TestInsertRow($col); ?>
-                </tr>
+                <tr><th> <?php echo $col; ?> </th></tr>
               </thead>
               <tbody>
           <?php foreach ($data as $row) { ?>
@@ -50,8 +50,7 @@ if (isset($_POST['submit'])) {
               </tbody>
           </table>
           <?php } else { ?>
-            > No results found for <?php echo $col; ?>.
-          
+            No results found for <?php echo $col; ?>.
           <?php }
 
     } catch (PDOException $error) {
