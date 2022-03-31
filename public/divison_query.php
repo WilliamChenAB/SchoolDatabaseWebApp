d01f6e7b137b05126ce76d12287a2a4b631b89c9
Sean Bocirnea <sean@aetheris.ca>
2022-03-31 15:50:17 -0700

    division

---
+++ public/divison_query.php
@@ -0,0 +1,62 @@
+<!-- Get a list of sponsor names who sponsor all teams with donation >= $N$ -->
+
+<?php require "templates/header.php"; ?>
+
+<?php
+
+require "data/config.php";
+
+function sponsorsTable ($result, $tableName) {
+    $data = $result->fetchAll();
+    if ($data && $result->rowCount() > 0) { ?>
+        <h2><?php echo $tableName; ?></h2>
+    
+        <table>
+          <thead>
+            <tr>
+            <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+      <?php foreach ($data as $row) { ?>
+          <tr>
+            <td><?php echo $row["name"]; ?></td>
+          </tr>
+        <?php } ?>
+          </tbody>
+      </table>
+      <?php } else { ?>
+        No results found!
+      <?php }
+}
+
+if (isset($_POST['submit'])) {
+    try {
+        // Create connection
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT name FROM Sponsors S WHERE NOT EXISTS (SELECT T.tid FROM Teams T WHERE NOT EXISTS (SELECT R.tid FROM Sponsorship R WHERE R.tid = T.tid AND R.sid = S.sid AND R.donation >= :amt))";
+
+        $amt = $_POST['donation'];
+
+        $result = $connection->prepare($sql);
+        $result->bindParam(':amt', $amt, PDO::PARAM_INT);
+        $result->execute();
+
+        sponsorsTable ($result, "Result:");
+
+    } catch (PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+
+<h2>Find the names of all sponsors who donated $N or more to all teams:</h2>
+
+<form method="post">
+  <label for="donation">Donation Amount ($)</label>
+  <input type="number" name="donation" value="donation">
+  <input type="submit" name="submit" value="View Results">
+</form>
+
+<?php require "templates/footer.php"; ?>
--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@
     <li> <a href="update.php"><strong>Update</strong></a> - rename a team </li>
     <li> <a href="join_query.php"><strong>Join</strong></a> - find size of rinks </li>
     <li> <a href="projection_query.php"><strong>Projection</strong></a> - project venues </li>
+    <li> <a href="division_query.php"><strong>Division</strong></a> - sponsors who sponsor all teams </li>
     <li> <a href="select_query.php"><strong>Select</strong></a> - Select players with more than n goals </li>
     <li> <a href="aggregation.php"><strong>Aggregation</strong></a> - salaried player with the lowest salary </li>
     <li> <a href="nested_aggregation.php"><strong>Nested Aggregation</strong></a> - average goals per position </li>
